e18b42e3f3f76f5f972039f7cf622394ead52e1c
boethin <sebastian@boethin.eu>
2015-04-03 16:32:53 +0200

    added license information

--- include/gcc.h
+++ include/gcc.h
@@ -1,5 +1,24 @@
-#ifndef ___GCC_H___
-#define ___GCC_H__
+/**
+ * This file is part of intzip.
+ *
+ * Copyright (C) 2015 Sebastian Boethin <boethin@xn--domain.net>
+ *
+ * intzip is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * intzip is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with intzip.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#ifndef ___INTZIP_GCC_H___
+#define ___INTZIP_GCC_H___
 #ifdef __GNUC__
 /* Definitions for: GCC, the GNU Compiler Collection.
  *
