6c862a1a5758a37ae80edfcfd3899c01b3000844
boethin <sebastian@boethin.eu>
2015-04-13 01:38:44 +0200

    Unicode Code Points

--- unicode.sh
+++ unicode.sh
@@ -13,7 +13,7 @@ test -f "$ucd_gz" || curl "$ucd_url" | gzip -c9 - >$ucd_gz
 #   3400;<CJK Ideograph Extension A, First>;Lo;0;L;;;;;N;;;;;
 #   4DB5;<CJK Ideograph Extension A, Last>;Lo;0;L;;;;;N;;;;;
 first=
-cat "$ucd_gz" | gunzip -c - | cat UnicodeData.txt.gz | gunzip -c - | cut -f1,2 -d';' | while read s
+cat "$ucd_gz" | gunzip -c - | cut -f1,2 -d';' | while read s
 do
   n=$((16#${s%%;*}))
   case $s in
