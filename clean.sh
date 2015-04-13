1e033f7548e6987589dafb8b27f025c78a08812f
boethin <sebastian@boethin.eu>
2015-04-13 21:45:07 +0200

    update clean.sh

--- clean.sh
+++ clean.sh
@@ -4,13 +4,14 @@
 rm -vf config.guess config.h config.h.in~ config.log config.status config.sub configure
 rm -vf aclocal.m4 depcomp libtool stamp-h1 appxLogFile.log ltmain.sh install-sh missing compile test-driver
 rm -vfr autom4te.cache
-rm -f *.in
+rm -vf *.in
+rm -vf tests/testsuite tests/atlocal tests/atconfig
 find . -name Makefile -exec rm -vf {} \;
 find . -name Makefile.in -exec rm -vf {} \;
 for d in src lib tests
 do
 	rm -vf $d/*.o $d/*.lo
 	rm -vfr $d/.deps $d/.libs
-	rm -f $d/*.log
+	rm -vf $d/*.log
 done
 
